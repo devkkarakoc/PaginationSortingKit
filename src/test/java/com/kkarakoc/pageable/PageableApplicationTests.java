741cff9066fe57534267874f8737effd9cb6d1a5
krmkarakoc <keremkarakoc@ibaysoft.com.tr>
2024-12-25 16:11:03 +0300

    initial

---
+++ src/test/java/com/kkarakoc/pageable/PageableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkarakoc.pageable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
